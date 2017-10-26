4b236e3b500825f317b4b2a147a31bb0ee4aad28
Marco <33065355+RealShark82@users.noreply.github.com>
2017-10-26 16:43:47 +0200

    Add files via upload

--- multi.py
+++ multi.py
@@ -1,50 +1,165 @@
 #!/usr/bin/python
 import os
 import shutil
-import datetime as dt
 import time
 import Queue
 from threading import Thread
+import hashlib
 
 
-def do_work(q, curr_thread):
+def do_work(q, curr_thread, source_path):
 
     dest1_path = '/root/Documents/destcsv'
     dest2_path = '/root/Documents/destxls'
     dest3_path = '/root/Documents/desttxt'
     dest4_path = '/root/Documents/destof'
+    dest5_path = '/root/Documents/destcopy'
 
 
     while True:
+
         #get message from Queue
         filename = q.get()
+
         #compose the full path
-        date = dt.datetime.now().strftime('%Y%m%d%H%M%S%f')
-        source_full_path = ('{}/{}'.format(source_path,filename))
-        dest_filename = ('{}-{}'.format(date,filename))
+        source_full_path = os.path.join(source_path,filename)
+
         #csv
         if filename.endswith("csv"):
-            dest_full_path = ('{}/{}'.format(dest1_path,dest_filename))
-            shutil.move(source_full_path, dest_full_path)
+            dest_full_path = os.path.join(dest1_path,filename)
+
+            #check if a file already exist
+            #if not exist
+            if not os.path.isfile(dest_full_path):
+                shutil.move(source_full_path,dest_full_path)
+
+            #if exist
+            elif not hash(source_full_path) == hash(dest_full_path):
+
+                #add suffix i to file
+                i = 0
+                dest_full_path = os.path.join(dest1_path, "%d%s" %(i, filename))
+
+                #if exist suffix i++
+                while os.path.isfile(dest_full_path):
+                    i += 1
+                    dest_full_path = os.path.join(dest1_path,"%d%s" % (i,filename))
+                shutil.move(source_full_path,dest_full_path)
+
+            #move to duplicate file
+            else:
+                dest_full_path = os.path.join(dest5_path,filename)
+                shutil.move(source_full_path,dest_full_path)
             print '{} ha processato un file csv'.format(curr_thread)
+
         #xls
         elif filename.endswith("xls"):
-            dest_full_path = ('{}/{}'.format(dest2_path,dest_filename))
-            shutil.move(source_full_path, dest_full_path)
-            print '{} ha processato un file xls'.format(curr_thread)
+            dest_full_path = os.path.join(dest2_path,filename)
+
+            # check if a file already exist
+            # if not exist
+            if not os.path.isfile(dest_full_path):
+                shutil.move(source_full_path,dest_full_path)
+
+            # if exist
+            elif not hash(source_full_path) == hash(dest_full_path):
+
+                # add suffix i to file
+                i = 0
+                dest_full_path = os.path.join(dest2_path,"%d%s" % (i,filename))
+
+                # if exist suffix i++
+                while os.path.isfile(dest_full_path):
+                    i += 1
+                    dest_full_path = os.path.join(dest2_path,"%d%s" % (i,filename))
+                shutil.move(source_full_path,dest_full_path)
+
+            # move to duplicate file
+            else:
+                dest_full_path = os.path.join(dest5_path,filename)
+                shutil.move(source_full_path,dest_full_path)
+            print '{} ha processato un file csv'.format(curr_thread)
+
         #txt
         elif filename.endswith("txt"):
-            dest_full_path = ('{}/{}'.format(dest3_path,dest_filename))
-            shutil.move(source_full_path, dest_full_path)
-            print '{} ha processato un file txt'.format(curr_thread)
+            dest_full_path = os.path.join(dest3_path,filename)
+
+            # check if a file already exist
+            # if not exist
+            if not os.path.isfile(dest_full_path):
+                shutil.move(source_full_path,dest_full_path)
+
+            # if exist
+            elif not hash(source_full_path) == hash(dest_full_path):
+
+                # add suffix i to file
+                i = 0
+                dest_full_path = os.path.join(dest3_path,"%d%s" % (i,filename))
+
+                # if exist suffix i++
+                while os.path.isfile(dest_full_path):
+                    i += 1
+                    dest_full_path = os.path.join(dest3_path,"%d%s" % (i,filename))
+                shutil.move(source_full_path,dest_full_path)
+
+            # move to duplicate file
+            else:
+                dest_full_path = os.path.join(dest5_path,filename)
+                shutil.move(source_full_path,dest_full_path)
+            print '{} ha processato un file csv'.format(curr_thread)
+
         #other
         else:
-            dest_full_path = ('{}/{}'.format(dest4_path,dest_filename))
-            shutil.move(source_full_path, dest_full_path)
-            print '{} ha processato un file sconosciuto'.format(curr_thread)
+            dest_full_path = os.path.join(dest4_path,filename)
+
+            # check if a file already exist
+            # if not exist
+            if not os.path.isfile(dest_full_path):
+                shutil.move(source_full_path,dest_full_path)
+
+            # if exist
+            elif not hash(source_full_path) == hash(dest_full_path):
+
+                # add suffix i to file
+                i = 0
+                dest_full_path = os.path.join(dest4_path,"%d%s" % (i,filename))
+
+                # if exist suffix i++
+                while os.path.isfile(dest_full_path):
+                    i += 1
+                    dest_full_path = os.path.join(dest4_path,"%d%s" % (i,filename))
+                shutil.move(source_full_path,dest_full_path)
+
+            # move to duplicate file
+            else:
+                dest_full_path = os.path.join(dest5_path,filename)
+                shutil.move(source_full_path,dest_full_path)
+            print '{} ha processato un file csv'.format(curr_thread)
+
+
+def hash(fpth):
+
+    #create the hash of the file in fpth
+    with open(fpth, 'rb') as f:
+        m = hashlib.md5()
+
+        #read datablock of 8192
+        while True:
+            data = f.read(8192)
+            if not data:
+                break
+            m.update(data)
+
+        #return the hexhash
+        return m.hexdigest()
+    print m.hexdigest
+
+
 
 def main():
 
+    #define source directory
+    source_path = '/root/Documents/source'
     # queue initialization
     q = Queue.Queue()
 
@@ -54,16 +169,14 @@ def main():
     while  curr_thread < max_thread :
 
         #thread initialization
-        tw = Thread(target=do_work, args=(q, curr_thread))
+        tw = Thread(target=do_work, args=(q, curr_thread, source_path))
         tw.setDaemon(True)
         tw.start()
 
         #increment curr_thread
         curr_thread += 1
 
-    #define source directory
-    global source_path
-    source_path = '/root/Documents/source'
+    #put filename in Queue
     while True:
         for dirname,filename,filenames in os.walk(source_path):
             for filename in filenames:
